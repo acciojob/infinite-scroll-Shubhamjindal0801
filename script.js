fbdacc5ba44bb61a56ce6c1643ad0c32d1b0d408
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-25 00:18:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//your code here!
+const list = document.getElementById('infi-list');
+	list.addEventListener('scroll', function() {
+	  if (list.scrollHeight - list.scrollTop - list.clientHeight < 1) {
+		const newItem1 = document.createElement('li');
+		newItem1.innerText = `Item ${count}`;
+		list.appendChild(newItem1);
+		  count++;
+		const newItem2 = document.createElement('li');
+		newItem2.innerText = `Item ${count}`;
+		list.appendChild(newItem2);
+		  count++;
+	  }
+	});
 
+let count = 11;
